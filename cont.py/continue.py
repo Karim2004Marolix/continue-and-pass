1965a99b770d2989d7d13b31b51239907c172821
Karim2004Marolix <karim2004.marolix@gmail.com>
2023-09-08 09:18:53 +0530

    continue pass and

---
+++ cont.py/continue.py
@@ -0,0 +1,21 @@
+for i in range(10,20+1):
+    if i == 15:
+        break
+    else:
+        print(i)
+
+
+for i in range(10,20+1):
+    if i == 15:
+        continue
+    else:
+        print(i)
+
+
+
+
+for i in range(10,20+1):
+    if i == 15:
+        pass
+    else:
+        print(i)
